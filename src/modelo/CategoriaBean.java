f8e5cbfd8c726b88e4e536ab937714a61a32c1de
Carlos <diaztorrecillajuancarlos@gmail.com>
2020-12-02 20:22:29 -0600

    Funciones de Categoría

---
+++ src/modelo/CategoriaBean.java
@@ -0,0 +1,50 @@
+package modelo;
+
+public class CategoriaBean {
+    private int id;
+    private String nombre;
+    private String descripcion;
+
+    public CategoriaBean() {
+
+    }
+
+    public CategoriaBean(int id, String nombre, String descripcion) {
+        this.id = id;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+    }
+
+    public CategoriaBean(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    @Override
+    public String toString() {
+        return "MarcaBean{" +
+                "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                ", descripcion='" + descripcion + '\'' +
+                '}';
+    }
+}
---
+++ src/modelo/CategoriaDao.java
@@ -0,0 +1,136 @@
+package modelo;
+
+import servicio.general.ConexionMySQL;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class CategoriaDao extends ConexionMySQL {
+    private Connection con;
+    private PreparedStatement ps;
+    private ResultSet rs;
+    private final String SQL_INSERTAR_CATEGORIA = "INSERT INTO categoria (nombre, descripcion) VALUES(?, ?)";
+    private final String SQL_MODIFICAR_CATEGORIA = "UPDATE categoria SET nombre = ?, descripcion = ? WHERE id= ?";
+    private final String SQL_ELIMINAR_CATEGORIA = "DELETE FROM categoria WHERE id = ?";
+    private final String SQL_CONSULTAR_CATEGORIA = "SELECT * FROM categoria ORDER BY nombre";
+    private final String SQL_CANTIDAD_CATEGORIA = "SLECT count(nombre) as cantidad FROM categoria";
+    public boolean add(CategoriaBean bean)  {
+        boolean resultado = false;
+
+        try{
+            ps = getConexion().prepareStatement(SQL_INSERTAR_CATEGORIA);
+            ps.setString(1, bean.getNombre());
+            ps.setString(2, bean.getDescripcion());
+            resultado = ps.executeUpdate() == 1;
+
+        }catch (SQLException e){
+            System.out.println("Error en CategoriaDao().add(CategoriaBean bean) :(" );
+            System.out.println(e);
+
+        }finally {
+            try{
+                ps.close();
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+
+        }
+        return resultado;
+    }
+
+
+    public boolean update(CategoriaBean bean){
+        boolean resultado = false;
+
+        try{
+            ps = getConexion().prepareStatement(SQL_MODIFICAR_CATEGORIA);
+
+            ps.setString(1, bean.getNombre());
+            ps.setString(2,bean.getDescripcion());
+            ps.setInt(3,bean.getId());
+            resultado = ps.executeUpdate() == 1;
+        }catch (SQLException e){
+            System.out.println("Error en CategoriaDao().update(CategoriaBean bean) :(");
+            System.out.println(e);
+        }finally {
+            try{
+                ps.close();
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+        return resultado;
+    }
+
+
+    public boolean delete(int id){
+        boolean resultado = false;
+
+        try{
+            ps = getConexion().prepareStatement(SQL_ELIMINAR_CATEGORIA);
+            ps.setInt(1, id);
+            resultado = ps.executeUpdate() == 1;
+
+        }catch (SQLException e){
+            System.out.println("Error en CategoriaDao().delete(CategoriaBean bean) :(");
+            System.out.println(e);
+
+        }finally {
+            try{
+                ps.close();
+
+            }catch (Exception e){
+                System.out.println("Error al cerrar las conexiones :(");
+
+            }
+        }
+        return resultado;
+    }
+
+    public CategoriaBean [] query(){
+        CategoriaBean[] categoria = null;
+
+        try{
+            ps = getConexion().prepareStatement(SQL_CANTIDAD_CATEGORIA);
+            rs = ps.executeQuery();
+
+            if(rs.next()){
+                categoria = new CategoriaBean[rs.getInt("cantidad")];
+            }
+
+        }catch (SQLException e) {
+            System.out.println("Error al consultar la cantidad en categoria :( ");
+            System.out.println(e);
+        }
+        if(categoria != null){
+            try{
+                ps = getConexion().prepareStatement(SQL_CONSULTAR_CATEGORIA);
+                rs = ps.executeQuery();
+
+                for(int i=0; rs.next(); i++)
+                    categoria[i] = new CategoriaBean(
+                            rs.getInt("id"),
+                            rs.getString("nombre"),
+                            rs.getString("descripcion")
+                    );
+
+            }catch (Exception e){
+                System.out.println("Error en MarcaDao().query()");
+                System.out.println(e);
+            }finally {
+                try{
+                    ps.close();
+                }catch (Exception e){
+                    System.out.println("Error al cerrar las conexiones :(");
+                    System.out.println(e);
+
+                }
+            }
+        }
+        return categoria;
+
+    }
+
+}
