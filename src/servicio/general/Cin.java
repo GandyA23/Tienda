0fcec9298e1ed12f172d82c5b0187a76c59e27e5
GandyA23 <avila.gandy@gmail.com>
2020-12-07 19:38:15 -0600

    Cambio de lectura, se valida que ingrese enteros o doubles

--- src/servicio/general/Cin.java
+++ src/servicio/general/Cin.java
@@ -5,16 +5,46 @@ import java.util.Scanner;
 public class Cin {
     private Scanner cin ;
 
-    public void init(){
+    private void init(){
         cin = new Scanner(System.in);
     }
 
-    public int cInt(){
-        return cin.nextInt();
+    public int cInt(String mensaje){
+        int aux = 0 ;
+        boolean c = true ;
+
+        do{
+            init();
+            try{
+                System.out.print(mensaje);
+                aux = cin.nextInt();
+
+                c = false ;
+            }catch (Exception e){
+                System.out.println("Error: No ingrese caracteres, vuelva a intentarlo.");
+            }
+        }while(c);
+
+        return aux;
     }
 
-    public double cDouble(){
-        return cin.nextDouble();
+    public double cDouble(String mensaje){
+        double aux = 0 ;
+        boolean c = true ;
+
+        do{
+            init();
+            try{
+                System.out.print(mensaje);
+                aux = cin.nextDouble();
+
+                c = false ;
+            }catch (Exception e){
+                System.out.println("Error: No ingrese caracteres, vuelva a intentarlo.");
+            }
+        }while(c);
+
+        return aux;
     }
 
     public String cString(){
